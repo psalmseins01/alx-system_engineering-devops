35818fea2d6b6f40b4f906c1f2f6015cef68a9af
Anthony Gbobo <psalmseins@gmail.com>
2024-01-30 16:27:10 -0800

    Ruby script that accepts one argument and pass it to a regular expression matching method

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,3 +1,5 @@
 #!/usr/bin/env ruby
-#
-#puts ARGV[0].scan(/hb?t?n/).join
+# Ruby script that accepts one argument
+# and pass it to a regular expression matching method
+
+puts ARGV[0].scan(/hb?t?n/).join
