c24b3bc7c8907a211bab01cb05d0a09cf8c9aee6
Anthony Gbobo <psalmseins@gmail.com>
2024-02-01 15:28:12 -0800

    Describe MAC address

--- 0x07-networking_basics/git.sh
+++ 0x07-networking_basics/git.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-git add . && git commit -m "LAN connect local devices together, WAN connects LANs together" && git push
+git add . && git commit -m "Describe MAC address" && git push
