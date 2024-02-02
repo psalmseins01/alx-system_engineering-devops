90a1b88f166fe77045bfd71955bf1e52d086a0a7
Anthony Gbobo <psalmseins@gmail.com>
2024-02-02 15:49:36 -0800

    Bash script that listens on port 98 on localhost

--- 0x08-networking_basics_2/git.sh
+++ 0x08-networking_basics_2/git.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-git add . && git commit -m "Bash script that listens on port 98 on localhost" && git pusih
+git add . && git commit -m "Bash script that listens on port 98 on localhost" && git push
