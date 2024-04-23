601f79ab98ed8c5188dc53a1048783f9c305130d
Anthony Gbobo <psalmseins@gmail.com>
2024-04-22 17:24:07 -0700

    Task 1

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Using REST API to gather employee data
+"""
+
+import re
+import requests
+import sys
+
+REST_API = "https://jsonplaceholder.typicode.com"
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            response = requests.get('{}/users/{}'.format(REST_API, id)).json()
+            req_task = requests.get('{}/todos'.format(REST_API)).json()
+            emp_name = response.get('name')
+            tasks = list(filter(lambda x: x.get('UserId') == id, req_task))
+            finished_task = list(filter(lambda x: x.get('completed'), tasks))
+            print('Employee {} is done with tasks ({}/{}):'.format(
+                    emp_name,
+                    len(finished_task),
+                    len(tasks)
+                )
+            )
+            if len(finished_task) > 0:
+                for task in finished_task:
+                    print('\t {}'.format(task.get('title')))
