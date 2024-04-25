36795a7ce3e914b0ec01aec4981340a1445c03aa
Anthony Gbobo <psalmseins@gmail.com>
2024-04-24 18:16:42 -0700

    Task 4

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Script to fetch REST API for employees todo lists"""
+
+import requests
+import json
+import sys
+
+
+if __name__ == "__main__":
+    server_addr = "https://jsonplaceholder.typicode.com/users"
+    response = requests.get(server_addr)
+    Users = response.json()
+
+    users_dict = dict()
+    for user in Users:
+        USER_ID = user.get('id')
+        USERNAME = user.get('username')
+        server_addr = "https://jsonplaceholder.typicode.com/users/{}".format(USER_ID)
+        url = server_addr + '/todos'
+        response = requests.get(url)
+
+        tasks = response.json()
+        users_dict[USER_ID] = []
+        for task in tasks:
+            TASK_COMPLETED_STATUS = task.get('completed')
+            TASK_TITLE = task.get('title')
+            users_dict[USER_ID].append({
+                "task": TASK_TITLE,
+                "completed": TASK_COMPLETED_STATUS,
+                "username": USERNAME
+            })
+    """Dumping into json"""
+    with open('todo_all_employees.json', 'w') as hd:
+        json.dump(users_dict, hd)
