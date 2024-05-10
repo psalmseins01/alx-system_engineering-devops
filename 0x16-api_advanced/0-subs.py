61a835101d72f9edbf87aae279a0e2fc1e91087c
Anthony Gbobo <psalmseins@gmail.com>
2024-05-09 21:53:55 -0700

    Task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,28 @@
-#!/usr/bin/python3
+import requests
+import argv
 '''
     Module contains that defines a function
     'number_of_subscribers'
 '''
-import requests
-from sys import argv
 
 
 def number_of_subscribers(subreddit):
+
     '''
        This function returns the total number of subscribers
        for a subreddit
     '''
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {
-        "User-Agent": "Linux: 0x16.api.advanced"
-        }
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != requests.codes.ok:
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-    results = response.json().get('data')
-    return results.get('subscribers')
 
 
 if __name__ == "__main__":
